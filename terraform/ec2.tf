c03eeccd9f043b891413d72ee0de3f26708568d6
dvdebugger <missjoode18@gmail.com>
2022-09-14 14:36:59 +0300

    change in ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "ansible-ec2" {
-  ami             = "ami-005e54dee72cc1d00" 
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.sg.id]
+resource "aws_instance" "ansible" {
+  ami             = "ami-052efd3df9dad4825" 
+  instance_type   = "t2.mediume"
+  security_groups = [aws_security_group.sg1.id]
 
   user_data = <<-EOF
   #!/bin/bash
@@ -22,10 +22,10 @@ resource "aws_instance" "ansible-ec2" {
   } 
 }
 
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "sg1" {
   name        = "allow_ssh_http"
   description = "Allow ssh http inbound traffic"
-  vpc_id      = aws_vpc.app_vpc.id
+ 
 
   ingress {
     description      = "SSH from VPC"
@@ -48,5 +48,43 @@ resource "aws_security_group" "sg" {
 
   tags = {
     Name = "allow_ssh_http"
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami             = "ami-052efd3df9dad4825" 
+  instance_type   = "t2.mediume"
+  security_groups = [aws_security_group.sg2.id]
+
+  tags = {
+    Name = "web_instance"
+  }
+
+  volume_tags = {
+    Name = "web_instance"
+  } 
+}
+
+resource "aws_security_group" "sg2" {
+  name        = "allow_ssh_http"
+  description = "Allow ssh http inbound traffic"
+ 
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
